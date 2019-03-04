51b12c843d404acc238e41de144289cce5c6d432
EugeneListarevich <yauheni.listarevich@itechart-group.com>
2019-03-04 14:21:25 +0300

    changed html and css by BEM

--- projects/admin-portal/src/app/app-routing.module.ts
+++ projects/admin-portal/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from 'projects/shared/src/lib/shared.module';
 import { LoginComponent, BuildingComponent } from 'projects/shared/src/public_api';
-import { HomeComponent } from './components/home/home.component';
 import { FloorsResolveService } from 'projects/shared/src/lib/services/floors-resolve/floors-resolve.service';
 
 const routes: Routes = [
