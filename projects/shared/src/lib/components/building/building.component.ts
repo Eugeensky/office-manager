bf40d2968da918ef3612037802789de91772a581
EugeneListarevich <yauheni.listarevich@itechart-group.com>
2019-03-29 14:23:21 +0300

    created comp

--- projects/shared/src/lib/components/building/building.component.ts
+++ projects/shared/src/lib/components/building/building.component.ts
@@ -8,19 +8,18 @@ import { FloorInfo } from '../../models/floor-info';
   styleUrls: ['./building.component.scss']
 })
 export class BuildingComponent implements OnInit {
-  public maxWidth: string;
+  public floorsInfo: FloorInfo[];
+
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.data.subscribe(data => {
       this.floorsInfo = data.floorsInfo;
       this.floorsInfo.reverse();
     });
   }
-
-  public floorsInfo: FloorInfo[];
+  
   ngOnInit() {
-    const width = ((document.documentElement.clientHeight - 140) / (this.floorsInfo.length + 3)) * 6;
-    this.maxWidth = width + 'px';
   }
+
   public goToFloor(floorNumber: number) {
     this.router.navigateByUrl(`floors/${floorNumber}`);
   }
