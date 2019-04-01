ed7b8306a444f3f1fe14fccdd47d22b540339965
EugeneListarevich <yauheni.listarevich@itechart-group.com>
2019-04-01 14:41:12 +0300

    redesigned building with floors scrolling, added floors navigation buttons, deleted old images

--- projects/shared/src/lib/components/building/building.component.ts
+++ projects/shared/src/lib/components/building/building.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FloorInfo } from '../../models/floor-info';
+import {HostListener} from '@angular/core';
 
 @Component({
   selector: 'shared-building',
   templateUrl: './building.component.html',
   styleUrls: ['./building.component.scss']
 })
-export class BuildingComponent implements OnInit {
+export class BuildingComponent implements AfterViewInit {
+
+  @ViewChild('floorScroll') floorScroll: ElementRef;
   public floorsInfo: FloorInfo[];
+  public isScrollOnBottom: boolean;
+  public isScrollOnTop: boolean;
 
   constructor(private route: ActivatedRoute, private router: Router) {
     this.route.data.subscribe(data => {
@@ -16,11 +21,69 @@ export class BuildingComponent implements OnInit {
       this.floorsInfo.reverse();
     });
   }
-  
-  ngOnInit() {
+
+  ngAfterViewInit() {
+    const floorsCount = this.floorsInfo.length;
+    const floorsOverScroll = floorsCount - 5;
+    this.floorScroll.nativeElement.onscroll = () => {
+      if (this.floorScroll.nativeElement.scrollTop >
+        ((this.floorScroll.nativeElement.scrollHeight / floorsCount) * floorsOverScroll - 5)) {
+        this.isScrollOnBottom = true;
+      } else {
+          this.isScrollOnBottom = false;
+      }
+
+      if (this.floorScroll.nativeElement.scrollTop < 5) {
+        this.isScrollOnTop = true;
+      } else {
+        this.isScrollOnTop = false;
+      }
+    };
+
+    this.floorScroll.nativeElement.scrollTop = (this.floorScroll.nativeElement.scrollHeight / floorsCount) * floorsOverScroll;
+  }
+
+  public scrollUp() {
+    if (this.isScrollOnTop) {
+      return;
+    }
+    const floorsCount = this.floorsInfo.length;
+    const oneFloorHeight = this.floorScroll.nativeElement.scrollHeight / floorsCount;
+    const currentFloor = Math.trunc(this.floorScroll.nativeElement.scrollTop / (oneFloorHeight + 5));
+    const newScrollTop = currentFloor * oneFloorHeight;
+    let i = 1;
+    for (let currentScrollTop = this.floorScroll.nativeElement.scrollTop; currentScrollTop > newScrollTop - 1; currentScrollTop--) {
+      setTimeout(() => this.floorScroll.nativeElement.scrollTop = currentScrollTop, i);
+      i++;
+    }
+  }
+
+  public scrollDown() {
+    if (this.isScrollOnBottom) {
+      return;
+    }
+    const floorsCount = this.floorsInfo.length;
+    const oneFloorHeight = this.floorScroll.nativeElement.scrollHeight / floorsCount;
+    const currentFloor = Math.trunc(this.floorScroll.nativeElement.scrollTop / (oneFloorHeight - 5));
+    const newScrollTop = (currentFloor + 1) * oneFloorHeight;
+    let i = 1;
+    for (let currentScrollTop = this.floorScroll.nativeElement.scrollTop; currentScrollTop < newScrollTop + 1; currentScrollTop++) {
+      setTimeout(() => this.floorScroll.nativeElement.scrollTop = currentScrollTop, i);
+      i++;
+    }
   }
 
   public goToFloor(floorNumber: number) {
     this.router.navigateByUrl(`floors/${floorNumber}`);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowUp') {
+      this.scrollUp();
+    }
+    if (event.key === 'ArrowDown') {
+      this.scrollDown();
+    }
   }
 }
